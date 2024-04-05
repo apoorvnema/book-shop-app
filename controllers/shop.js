11071e9d4b473d070e4e40186f03684105c7db4e
apoorvnema <apoorvnema38@gmail.com>
2024-04-06 00:25:10 +0530

    making cart feature

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -19,7 +20,6 @@ exports.getProduct = (req, res, next) => {
       path: '/products'
     });
   });
-  res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {
@@ -37,6 +37,14 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
